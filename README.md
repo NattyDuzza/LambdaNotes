75d49dc3f099f02f48eadeb1fdde27f6b2b595d4
Nathan Durrant <nathan.h.durrant@gmail.com>
2023-02-26 16:45:53 +0000

    Finished PrelimUI design

---
+++ README.md
@@ -0,0 +1,9 @@
+# LambdaNotes
+
+## Introduction
+
+LambdaNotes is a piece of software created for my A-Level Computer Science NEA.
+
+The code is yet to be refactored and, as such, may not be elegant to more experienced programmer. This is of not of worry to me however; the aim of this is to simply create a working prototype upon which I may, or may not, work on in the years to come.
+
+Please see the rest of the README for details on how to use and navigate the program.
--- code/PrelimUI.py
+++ code/PrelimUI.py
@@ -14,18 +14,29 @@ class MainRemovalWin(tk.Tk):
     def __init__(self, flashcards):
         super().__init__() #initialises tk class which has been inherited
         self.multiPick = False
-        self.rightFrame = tk.Frame(self)
-        self.leftFrame = tk.Frame(self)
 
-        #configuration
+        #window configuration
         self.title('LambdaNotes - Remove Flashcard')
         self.geometry('400x400')
 
-        #frames
-        self.rightFrame.grid(row=0, column=0, sticky=tk.NSEW)
-        self.leftFrame.grid(row=0, column=0, sticky=tk.NSEW)
+        #----------------------------------------------------
 
-        backButton = tk.Button(self.leftFrame, text="Back", command = self.backButton)
+        #frames
+        self.topBarFrame = tk.Frame(self)
+        self.rightFrame = tk.Frame(self)
+        self.leftFrame = tk.Frame(self)
+        self.multiPickFrame = tk.Frame(self.rightFrame)
+
+        self.topBarFrame.grid(row=0, column=0, sticky=tk.NSEW)
+        self.rightFrame.grid(row=1, column=1, sticky=tk.NSEW)
+        self.leftFrame.grid(row=1, column=0, sticky=tk.NSEW)
+        self.multiPickFrame.grid(row=0, column=0, sticky=tk.NSEW)
+
+        #-------------------------------------------------------------------------------------
+        
+        #elements
+
+        backButton = tk.Button(self.topBarFrame, text="Back", command = self.backButton)
         backButton.grid(row=0, column=0, padx=(2,2), pady=(2,10), sticky=tk.W)
 
         flashcardList = tk.Listbox(self.leftFrame)
@@ -37,16 +48,20 @@ class MainRemovalWin(tk.Tk):
         flashcardList.config(yscrollcommand = flashcardListScroll.set)
         flashcardListScroll.config(command = flashcardList.yview)
         
-        multiPickLabel = tk.Label(self.rightFrame, text="Mutlipick:")
-        multiPickLabel.grid(row=0, column=0, sticky=tk.E)
 
-        multiPickCheck = tk.Checkbutton(self.rightFrame, variable=self.multiPick)
-        multiPickCheck.grid(row=1, column=0, sticky=tk.W)
+        multiPickLabel = tk.Label(self.multiPickFrame, text="Mutlipick:")
+        multiPickLabel.grid(row=0, column=0, pady=(20, 5), sticky=tk.E)
 
+        multiPickCheck = tk.Checkbutton(self.multiPickFrame, variable=self.multiPick)
+        multiPickCheck.grid(row=0, column=1, pady=(20, 5), sticky=tk.W)
 
+        removeFlashcardBtn = tk.Button(self.rightFrame, text="Remove Flashcard(s)", command=self.removeFlashcard)
+        removeFlashcardBtn.grid(row=2, column=0, pady=(7,0))
 
-        removeFlashcardBtn = tk.Button(self, text="Remove Flashcard(s)", command= self.removeFlashcard)
-        removeFlashcardBtn.grid(row=2, column=0)
+        changeCardsetBtn = tk.Button(self.rightFrame, text="Charge Cardset", command=self.changeCardset)
+        changeCardsetBtn.grid(row=3, column=0, pady=(7,0))
+
+        #------------------------------------------------------------------------------------------------------------------
 
 #        for i in range(0, len(flashcards)):
  #           flashcardList.insert(tk.END, flashcards[i])
@@ -57,6 +72,9 @@ class MainRemovalWin(tk.Tk):
        pass
 
     def removeFlashcard(self):
+        pass
+
+    def changeCardset(self):
         pass
 
   
